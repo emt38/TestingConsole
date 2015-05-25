5b0fe412fc220a36d79fa92787580c39094cac4d
emt38 <ezequieltheboss64@gmail.com>
2015-05-24 22:33:12 -0400

    ProbandoRepositorios
    Un programita en consola

---
+++ TestingConsole/TestingConsole.cpp
@@ -0,0 +1,12 @@
+// TestingConsole.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine("Hello World");
+	Console::ReadLine();
+    return 0;
+}
